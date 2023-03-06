e14fa35924ebd062d9b7e26f8be622fd4ede2e7a
Josef Fröhle <github@josef-froehle.de>
2023-03-06 11:35:37 +0100

    platformio

---
+++ include/config.h
@@ -0,0 +1,58 @@
+// WiFi
+#define WIFI_SSID     "changeme" // Add wifi name
+#define WIFI_PASSWORD "changeme" // Add wifi passowrd
+
+#define ID "sourdough" // Add unique name for this sensor
+#define INTERVAL 60
+
+#define DHTPIN 32    // Which pin is DHT 11 connected to
+#define DHTTYPE DHT11 // Type DHT 11
+
+#define ALCOHOL_A_PIN A0
+#define ALCOHOL_D_PIN A10
+
+#define ULTRASONIC_PIN_TRIG 4 // Which pin is HC-SR04's trig connected to
+#define ULTRASONIC_PIN_ECHO 5 // Which pin is HC-SR04's echo connected to
+
+#define INFLUX_HOST "your_influx_host" // Influx host (e.g. eu-central-1-1.aws.cloud2.influxdata.com)
+#define INFLUX_ORG_ID "your_influx_org_id" // Org id
+#define INFLUX_TOKEN "Token your_token" // Influx token
+#define INFLUX_BUCKET "your_sourdough_bucket" // Influx bucket that we set up for this host
+
+#define LOKI_USER "111" // Hosted Loki user
+#define LOKI_API_KEY "api_key" // Hosted Loki API key
+
+// How to get the Root CA cert:
+// https://techtutorialsx.com/2017/11/18/esp32-arduino-https-get-request/
+
+#define ROOT_CA "-----BEGIN CERTIFICATE-----\n" \
+"MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw\n" \
+"TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n" \
+"cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4\n" \
+"WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu\n" \
+"ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY\n" \
+"MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc\n" \
+"h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+\n" \
+"0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U\n" \
+"A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW\n" \
+"T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH\n" \
+"B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC\n" \
+"B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv\n" \
+"KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn\n" \
+"OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn\n" \
+"jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw\n" \
+"qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI\n" \
+"rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV\n" \
+"HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq\n" \
+"hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL\n" \
+"ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ\n" \
+"3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK\n" \
+"NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5\n" \
+"ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur\n" \
+"TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC\n" \
+"jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc\n" \
+"oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq\n" \
+"4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA\n" \
+"mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d\n" \
+"emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=\n" \
+"-----END CERTIFICATE-----"
---
+++ src/main.cpp
@@ -0,0 +1,214 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <NTPClient.h>
+#include <WiFiClientSecure.h>
+#include <HTTPClient.h>
+#include <DHT.h>
+#include <HCSR04.h>
+
+#include "config.h"
+
+// NTP Client
+WiFiUDP ntpUDP;
+NTPClient ntpClient(ntpUDP);
+
+// DHT Sensor
+DHT dht(DHTPIN, DHTTYPE);
+
+// Ultrasonic Sensor
+UltraSonicDistanceSensor distanceSensor(ULTRASONIC_PIN_TRIG, ULTRASONIC_PIN_ECHO); // Initialize sensor that uses digital pins 4 and 5
+
+// Clients
+HTTPClient httpInflux;
+HTTPClient httpLoki;
+
+WiFiClientSecure *client;
+
+// Function to set up the connection to the WiFi AP
+void setupWiFi()
+{
+  Serial.print("Connecting to '");
+  Serial.print(WIFI_SSID);
+  Serial.print("' ...");
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("connected");
+
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  randomSeed(micros());
+
+  String body = String("sourdough_alcohol_limit value=1\n") +
+                "sourdough_heigh value=2";
+  // Submit POST request via HTTP
+  httpInflux.begin(String("https://nc.josef-froehle.de/"), ROOT_CA);
+  httpInflux.addHeader("Authorization", INFLUX_TOKEN);
+
+  int httpCode2 = httpInflux.POST(body);
+  if (httpCode2 > 0)
+  {
+    Serial.printf("Influx [HTTPS] POST...  Code: %d  Response: ", httpCode2);
+    Serial.println(httpInflux.getLocation());
+    Serial.println();
+  }
+  else
+  {
+    Serial.printf("Influx [HTTPS] POST... Error: %s\n", httpInflux.errorToString(httpCode2).c_str());
+  }
+
+  httpInflux.end();
+
+  client = new WiFiClientSecure;
+  if (client)
+  {
+    // set secure client with certificate
+    client->setCACert(ROOT_CA);
+  }
+
+  // Submit POST request via HTTP
+  httpInflux.begin(*client, String("https://nc.josef-froehle.de/"));
+  httpInflux.addHeader("Authorization", INFLUX_TOKEN);
+
+  int httpCode = httpInflux.GET();
+  if (httpCode > 0)
+  {
+    Serial.printf("Influx [HTTPS] GET...  Code: %d  Response: ", httpCode);
+    Serial.println();
+  }
+  else
+  {
+    Serial.printf("Influx [HTTPS] GET... Error: %s\n", httpInflux.errorToString(httpCode).c_str());
+  }
+
+  httpInflux.end();
+}
+
+// Function to submit metrics to locally run Influx
+void submitHostedInflux(unsigned long ts, float cels, float hum, float hic, long height, int alcohol_value, int alcohol_limit)
+{
+
+  // Build body
+  String body = String("temperature value=") + cels + " " + ts + "\n" +
+                "sourdough_humidity value=" + hum + " " + ts + "\n" +
+                "sourdough_index value=" + hic + " " + ts + "\n" +
+                "sourdough_alcohol_value value=" + alcohol_value + ts + "\n" +
+                "sourdough_alcohol_limit value=" + alcohol_limit + ts + "\n" +
+                "sourdough_heigh value=" + height + " " + ts;
+
+  // Submit POST request via HTTP
+  httpInflux.begin(*client, String("https://") + INFLUX_HOST + "/api/v2/write?org=" + INFLUX_ORG_ID + "&bucket=" + INFLUX_BUCKET + "&precision=s");
+  httpInflux.addHeader("Authorization", INFLUX_TOKEN);
+
+  int httpCode = httpInflux.POST(body);
+  if (httpCode > 0)
+  {
+    Serial.printf("Influx [HTTPS] POST...  Code: %d  Response: ", httpCode);
+    Serial.println();
+  }
+  else
+  {
+    Serial.printf("Influx [HTTPS] POST... Error: %s\n", httpInflux.errorToString(httpCode).c_str());
+  }
+
+  httpInflux.end();
+}
+
+// Function to submit metrics to Influx
+void submitLogsToLoki(unsigned long ts, float cels, float hum, float hic, long height, int alcohol_value, int alcohol_limit)
+{
+  String logMessage = String("temperature=") + cels + " humidity=" + hum + " heat_index=" + hic + " alcohol_value=" + alcohol_value + " alcohol_limit=" + alcohol_limit + " height=" + height + " info";
+  String logJson = String("{\"streams\": [{ \"stream\": { \"monitoring\": \"sourdough_monitoring\", \"monitoring_type\": \"sourdough\"}, \"values\": [ [ \"") + ts + "000000000\", \"" + logMessage + "\" ] ] }]}";
+  Serial.println(logJson);
+
+  // Submit POST request via HTTP
+  httpLoki.begin(String("https://") + LOKI_USER + ":" + LOKI_API_KEY + "@logs-prod-us-central1.grafana.net/loki/api/v1/push");
+  httpLoki.addHeader("Content-Type", "application/json");
+
+  int httpCode = httpLoki.POST(logJson);
+  if (httpCode > 0)
+  {
+    Serial.printf("Loki [HTTPS] POST...  Code: %d  Response: ", httpCode);
+    Serial.println();
+  }
+  else
+  {
+    Serial.printf("Loki [HTTPS] POST... Error: %s\n", httpLoki.errorToString(httpCode).c_str());
+  }
+
+  httpLoki.end();
+}
+
+//  Function called at boot to initialize the system
+void setup()
+{
+  // Start the serial output at 115,200 baud
+  Serial.begin(115200);
+
+  // Connect to WiFi
+  setupWiFi();
+
+  // Initialize a NTPClient to get time
+  ntpClient.begin();
+
+  // Start the DHT sensor
+  dht.begin();
+}
+
+// Function called in a loop to read temp/humidity and submit them to hosted metrics
+void loop()
+{
+  // Reconnect to WiFi if required
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    WiFi.disconnect();
+    yield();
+    setupWiFi();
+  }
+
+  // Update time via NTP if required
+  while (!ntpClient.update())
+  {
+    yield();
+    ntpClient.forceUpdate();
+  }
+
+  // Get current timestamp
+  unsigned long ts = ntpClient.getEpochTime();
+
+  // Read humidity
+  float hum = dht.readHumidity();
+  yield();
+
+  // Read temperature as Celsius (the default)
+  float cels = dht.readTemperature();
+  yield();
+
+  double height = 18 - distanceSensor.measureDistanceCm();
+
+  int alcohol_value = analogRead(ALCOHOL_A_PIN);
+  int alcohol_limit = digitalRead(ALCOHOL_D_PIN);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(hum) || isnan(cels) || isnan(height))
+  {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(cels, hum, false);
+
+  yield();
+  submitHostedInflux(ts, cels, hum, hic, height, alcohol_value, alcohol_limit);
+  submitLogsToLoki(ts, cels, hum, hic, height, alcohol_value, alcohol_limit);
+
+  // wait INETRVAL s, then do it again
+  delay(INTERVAL * 1000);
+}
